0bbc9fcfb79b41029274a8c00554e145c8a4246f
alexandernoack <alexander.noack@amotiq.de>
2024-06-22 17:58:58 +0200

    cleanup

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
+
   return (
     <div>
         <h3>Add  new Transaction</h3>
         <form>
             <div className='form-control'>
                 <label htmlFor='text'>Text</label>
-                <input type='text' placeholder='Enter text...'/>
+                <input type='text' value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...'/>
             </div>
             <div className='form-control'>
                 <label htmlFor='amount'>Amount <br/>(negative - expence, positive - income)</label>
-                <input type='number' placeholder='Enter amount...'/>
+                <input type='number' value={amount} onChange={(e) => setAmount(e.amount.value)} placeholder='0'/>
             </div>
             <button className='btn'>Add Transaction</button>
         </form>
